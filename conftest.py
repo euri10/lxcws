b0c527fc633fd90dcc575e9221c184120c5f274f
euri10 <benoit.barthelet@gmail.com>
2017-04-27 08:36:19 +0200

    Added the meta_container fixture that spawns various distrib / releases

--- conftest.py
+++ conftest.py
@@ -8,12 +8,10 @@ logging.basicConfig()
 logging.root.setLevel(level=logging.DEBUG)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='module')
 def temp_container(request):
-    container_name = getattr(
-        request.session, "container_name", str(uuid.uuid1()))
+    container_name = getattr(request.module, "cccc", str(uuid.uuid1()))
     logger.debug('begin {}'.format(container_name))
-
     c = lxc.Container(container_name)
     if c.defined:
         logger.info('Container already exists')
@@ -35,3 +33,45 @@ def temp_container(request):
         c.stop()
     c.destroy()
     logger.debug('end {}'.format(container_name))
+
+
+
+d= {'debian': (['jessie', 'stretch', 'sid'], ['amd64']),
+    'ubuntu': (['xenial', 'yakkety', 'zesty'], ['amd64', 'i386'])}
+
+dist_permut = [(dist, release, arch) for dist, v in d.items() for release in v[0] for arch in v[1]]
+
+@pytest.fixture(scope='module', params=dist_permut, ids=['{}-{}-{}'.format(d, r, a) for d,r,a in dist_permut])
+def dist(request):
+    yield request.param
+
+
+@pytest.fixture(scope='module')
+def meta_container(dist):
+    print(dist)
+    container_name = str(uuid.uuid1())
+    logger.debug('begin {}'.format(container_name))
+    c = lxc.Container(container_name)
+    if c.defined:
+        logger.info('Container already exists')
+    else:
+        # Create the container rootfs
+
+        logger.debug(
+            'Create the container with dist: {} '.format(dist))
+        if not c.create(
+                'download', lxc.LXC_CREATE_QUIET,
+                {'dist': dist[0], 'release': dist[1],
+                 'arch': dist[2]}
+        ):
+            logger.debug('Failed to create the container rootfs')
+        else:
+            logger.info('Created the container rootfs')
+            # Save the configuration
+            c.save_config()
+    if not c.running:
+        c.start()
+    yield c
+    if c.running:
+        c.stop()
+    c.destroy()
